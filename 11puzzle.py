c6854b9853e5ccaac3d4e94d0151cdfce3f0db27
tg1625 <35944491+tg1625@users.noreply.github.com>
2020-10-18 14:45:52 -0400

    read states from file

---
+++ 11puzzle.py
@@ -0,0 +1,40 @@
+def createStates(filepath):
+    initialS = [[]] #initial state matrix
+    goalS = [[]] #goal state matrix
+    with open(filepath, 'r') as fp:
+        char = '' #keeping track of number
+        row = 0 #keeping track of matrix row
+        addingM = initialS #first adding to initial state matrix 
+        for c in fp.read():
+            if c.isnumeric():
+                char += c
+            elif c == ' ':
+                addingM[row].append(int(char))
+                char = ''
+            elif c == '\n':
+                if (char != ''): #add any found numbers 
+                    addingM[row].append(int(char))
+                    char = ''
+                if row == 3: #switch to adding to goal state matrix
+                    addingM = goalS
+                    row = 0
+                else:
+                    row += 1 #move to a new row 
+                    addingM.append([ ])
+        if char != '':
+            addingM[row].append(int(char)) #getting last character we may have missed
+    #cleaning up any extra rows, probably a better way of making sure we just don't have to do this
+    initialS = initialS[:3]
+    goalS = goalS[:3]
+    # print(initialS)
+    # print(goalS)
+    return initialS, goalS
+
+
+def main():
+    initialS, goalS = createStates("Sample_Input.txt")
+
+if __name__ == "__main__":
+    main()
+
+    
