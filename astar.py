bf03226f11ddf961972988c27ec6be84ff302dfd
hjx201 <hjx201@nyu.edu>
2020-10-25 17:56:17 -0400

    despacito

--- astar.py
+++ astar.py
@@ -1,23 +1,34 @@
-#each node of search tree stores action cost, sum of manhattan distances, and total h(n) value, and pointers to child nodes, is_expanded value?
+import heapq
+from copy import deepcopy
 
-#keep hashmap of evaluated states? 
-width = 4	
+width = 4
 height = 3
 
 
+
 class Node:
-	def __init__(self, state, fn, depth):
-		#initialize node
-		self.state = state
-		self.fn = fn
-		self.depth = depth
-		
-	def expand(self):
-		#create child node for left, right, down, up
-		
+        def __init__(self, state, goal, depth, solnpath, flist):
+                self.state = state                              #2-d matrix of puzzle position
+        
+                self.hn = sumManhattans(self.state, goal)       #stored to check if this is goal node
+                self.depth = depth #keeps track of depth in tree
+
+                self.fn = self.hn + self.depth #keeps track of total f(n)
+
+                self.solnpath = solnpath #keeps track of current solution path
+                self.flist = flist + str(self.fn) + " " #keeps track of list of fvalues of nodes generated in trees
+
+        def __str__(self):
+                return "(" + (str)(self.state) + ", f(n) = " + (str)(self.fn) + ", solution path = " + self.solnpath + ", f-list = " + self.flist + ")"
+
+        def __repr__(self):
+                return (str)(self)
+
+        def __lt__(self, other):
+                return self.fn < other.fn
 
 
-def createStates(filepath):
+def createStates(filepath): #create 2D matrices for initial state and goal state from file
     initialS = [[]] #initial state matrix
     goalS = [[]] #goal state matrix
     with open(filepath, 'r') as fp:
@@ -49,7 +60,64 @@ def createStates(filepath):
     # print(goalS)
     return initialS, goalS
 
-def printOutput(initialS, goalS):
+def expand(nodeo, goal, expanded, expandable): #create 4 children of the node and push them into the expandable list
+        if(nodeo.hn == 0): # check if goal has been reached
+                return expandable, True, nodeo
+        
+        
+        zeroy, zerox = findzero(nodeo.state) #find location of zero
+
+        rstate = deepcopy(nodeo.state)
+        lstate = deepcopy(nodeo.state)
+        ustate = deepcopy(nodeo.state)
+        dstate = deepcopy(nodeo.state) 
+
+        print(zeroy, zerox)
+
+        if(zerox > 0):
+                print("left") #make left state
+                lstate[zeroy][zerox], lstate[zeroy][zerox-1] = lstate[zeroy][zerox-1], lstate[zeroy][zerox]
+
+
+                #if not already in list with lower f(n) value
+                if lstate not in expanded:
+                        print(lstate)
+                        lnode = Node(lstate, goal, nodeo.depth + 1, nodeo.solnpath + "L ", nodeo.flist)                #create node and push into stack
+                        heapq.heappush(expandable,lnode)
+
+        #do the same for the other three
+        if (zerox < width - 1):
+                print("right") #make right
+                rstate[zeroy][zerox], rstate[zeroy][zerox+1] = rstate[zeroy][zerox+1], rstate[zeroy][zerox]
+                if rstate not in expanded:
+                        print(rstate)
+                        rnode = Node(rstate, goal, nodeo.depth + 1, nodeo.solnpath + "R ", nodeo.flist)
+                        heapq.heappush(expandable,rnode)
+        if (zeroy > 0):
+                print("up") #make up
+                ustate[zeroy][zerox], ustate[zeroy - 1][zerox] = ustate[zeroy - 1][zerox], ustate[zeroy][zerox]
+                if ustate not in expanded:
+                        print(ustate)
+                        unode = Node(ustate, goal, nodeo.depth + 1, nodeo.solnpath + "U ", nodeo.flist)
+                        heapq.heappush(expandable,unode)
+        if (zeroy < height - 1): #down
+                print("down")
+                dstate[zeroy][zerox], dstate[zeroy + 1][zerox] = dstate[zeroy + 1][zerox], dstate[zeroy][zerox]
+                if dstate not in expanded:
+                        print(dstate)
+                        dnode = Node(dstate, goal, nodeo.depth + 1, nodeo.solnpath + "D ", nodeo.flist)
+                        heapq.heappush(expandable, dnode)
+
+        return expandable, False, None
+
+
+def findzero(state): #find location of zero
+        for y in range(height):
+                for x in range(width):
+                        if(state[y][x] == 0):
+                                return y, x
+
+def printOutput(initialS, goalS): #print final output
     with open("Test_Output.txt", "w") as fp:
         #printing out initial state
         for row in initialS:
@@ -73,22 +141,34 @@ def sumManhattans(state1, state2):
     return dists
 
 def main():
-    initialS, goalS = createStates("Sample_Input.txt")
-    printOutput(initialS, goalS)
+        goalReached = False
+        goalNode = None
+
+        
+        initialS, goalS = createStates("Sample_Input.txt") #read in initial and goal states from starting node
+    
+        expandable = []
+        expanded = [] #initialize graphs of expandable nodes and already-expanded nodes
+
+        firstnode = Node(initialS, goalS, 0, "", "") #initialize node
+        
+        heapq.heappush(expandable,firstnode)#uses a min heap to get nodes with min f(n) value
+
+
+        while expandable and not goalReached: #while there are expandable nodes list isn't empty:
+                currnode = heapq.heappop(expandable) #expand node with least cost (uses min-heap)
+                print("expanding " + str(currnode.state))
+                expandable, goalReached, goalNode = expand(currnode, goalS, expanded, expandable) #push each child generated into expandable nodes list, along with its h(n) value and its current solution path and its cumulative f(n)s
+                expanded.append(currnode.state)
+
+                print(expandable)
+        #nodes are only created when we choose that state to be expanded
+
+        print(len(expanded))
+        print(expanded)
+        print("goal node: " + str(goalNode))
+        printOutput(initialS, goalS)
 
 if __name__ == "__main__":
-    main()
+        main()
 
-	#sum up manhattan distances
-
-#have another data structure to store path-cost pairs?
-
-
-#read in states from file
-#initialize graph of states (array of states? or dictionary map storing the h(n) and )
-#create tree-type data structure thing?
-#have list of unexpanded nodes (as a priority queue?)
-#attempt all options: create nodes for each thing
-#camculate h(n) of that state
-
-#expand_node: expands nodes and adds new states to the graph if they aren't there?
